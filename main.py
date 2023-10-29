dcade0d7bf859b414aa38b06960960823e93a256
Nikolai Matev <n.matev@hotmail.com>
2023-10-29 16:11:18 +0200

    Small Changes
    Minor changes with if else testing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 print("Welcome to my first game!")
 userName = input("What is your name? ")
-age = input("What's your age? ")
+age = int (input("What's your age? "))
 
 print("Hello", userName, "you are", age, "years old.")
+
+if age >= 18:
+    print("You are old enough!")
+elif age >= 14:
+    print("You can play with supervision.")
+else:
+    print("You are not old enough to play...")
